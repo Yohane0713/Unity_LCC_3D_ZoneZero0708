6e5717c565205d2d5bd4fc27bcb914e5031c9e8c
Guanyu.chen <tea005454@gmail.com>
2024-07-17 22:09:21 +0800

    Update.攻擊連段系統

---
+++ Assets/Scripts/ComboSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Mtaka
+{
+    /// <summary>
+    /// 連段系統：處理攻擊連段
+    /// </summary>
+    public class ComboSystem : MonoBehaviour
+    {
+        [SerializeField, Header("最大攻擊連段數"), Range(0, 10)]
+        private int comboMax;
+        [SerializeField, Header("攻擊連段時間"), Range(0, 2)]
+        private float breakComboTime = 1;
+
+        /// <summary>
+        /// 連段編號：-1 尚未開始 0 代表連段1
+        /// </summary>
+        private int comboIndex = -1;
+        private string parAttack = "觸發攻擊";
+        private string parComboValue = "攻擊連段數值";
+        private Animator ani;
+        private WaitForSeconds waitBreakComboTime;
+        private float[] animationsLength = { 1.33f, 1.58f, 1.66f, 2.23f };
+        private WaitForSeconds[] waitAnimationsTime;
+        private bool isAttacking;
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+            // 最佳化：初始化等待時間
+            waitBreakComboTime = new WaitForSeconds(breakComboTime);
+
+            waitAnimationsTime = new WaitForSeconds[comboMax];
+            for (int i = 0; i < comboMax; i++)
+            {
+                waitAnimationsTime[i] = new WaitForSeconds(animationsLength[i]);
+            }
+        }
+
+        private void Update()
+        {
+            ComboIndexHandle();
+        }
+
+        /// <summary>
+        /// 連段編號處理
+        /// </summary>
+        private void ComboIndexHandle()
+        {
+            if (isAttacking) return;
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                // 編號處理
+                comboIndex++;
+                // 如果連段編號 等於 連段最大值 就歸零
+                if (comboIndex == comboMax) comboIndex = 0;
+                print($"<color=#f36>連段編號：{comboIndex}</color>");
+                UpdateAnimation();
+                // 避免重複中斷，停止所有協同程序
+                StopAllCoroutines();
+                StartCoroutine(AttackTimeHandle());
+            }
+        }
+
+        /// <summary>
+        /// 更新動畫
+        /// </summary>
+        private void UpdateAnimation()
+        {
+            ani.SetTrigger(parAttack);
+            ani.SetFloat(parComboValue, comboIndex);
+        }
+
+        private IEnumerator AttackTimeHandle()
+        {
+            // 正在攻擊中
+            isAttacking = true;
+            yield return waitAnimationsTime[comboIndex];
+            // 攻擊結束
+            isAttacking = false;
+
+            yield return waitBreakComboTime;
+            comboIndex = -1;
+            print($"<color=#f33>中斷連擊：{comboIndex}</color>");
+        }
+    }
+}
